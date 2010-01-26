0f0b8d2f59d7e1636ca6485af29c9397071fb2e4
Ner'zhul <nerzhultheking@hotmail.fr>
2010-01-26 20:50:00 +0100

    [client rev 0.4.5d] cleanup delete not needed log
    Signed-off-by: Ner'zhul <nerzhultheking@hotmail.fr>

--- FSComClient/src/master.java
+++ FSComClient/src/master.java
@@ -7,7 +7,7 @@ public class master
 {
 
 	private static threading thr_lib;
-	final static String version = "Alpha 0.4.5c";
+	final static String version = "Alpha 0.4.5d";
 	
 	public master()	{}
 	
