5b8e3713d77240c219713551e034ec08cd2123ce
nerzhul <nerzhultheking@gmail.com>
2010-02-26 17:59:50 +0100

    [client rev 0.6.3e] serverlist fix finally the 4th mirror to be nerzhul's pc
    Signed-off-by: nerzhul <nerzhultheking@gmail.com>

--- FSComClient/src/master.java
+++ FSComClient/src/master.java
@@ -7,7 +7,7 @@ import thread.*;
 public class master
 {
 	private static threading thr_lib;
-	final static String version = "Beta 0.6.3d";
+	final static String version = "Beta 0.6.3e";
 	
 	public master()	{}
 	
